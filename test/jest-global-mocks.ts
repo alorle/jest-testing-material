f3f3410c8341e6bf415bf7f9e18e9fae8fc52469
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2021-10-19 23:10:00 +0200

    test: move from jasmine and karma to jest

---
+++ test/jest-global-mocks.ts
@@ -0,0 +1,4 @@
+import '@angular-builders/jest/dist/global-mocks/computed-style';
+import '@angular-builders/jest/dist/global-mocks/doctype';
+import '@angular-builders/jest/dist/global-mocks/match-media';
+import '@angular-builders/jest/dist/global-mocks/style-transform';
